beec367acdc13364607ec450cbf525ad61dc1e09
raphckrman <raphckrman@proton.me>
2026-08-27 00:51:04 +0200

    feat(constants): add BASE_URL

---
+++ src/constants.rs
@@ -0,0 +1,10 @@
+use std::sync::LazyLock;
+
+use url::Url;
+
+#[expect(
+    clippy::expect_used,
+    reason = "Hardcoded URL, validity guaranteed at compile time"
+)]
+pub static BASE_URL: LazyLock<Url> =
+    LazyLock::new(|| Url::parse("https://api-rest-prod.incb.fr/api/").expect("Invalid Base URL"));
