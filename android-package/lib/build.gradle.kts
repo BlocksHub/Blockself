e061ba1513340c1158e1e28e279ddb3cd926bedf
raphckrman <raphckrman@proton.me>
2026-08-28 19:23:16 +0200

    ci: fix target compatibility

--- android-package/lib/build.gradle.kts
+++ android-package/lib/build.gradle.kts
@@ -1,37 +1,15 @@
-plugins {
-    id("com.android.library") version "8.5.0"
-    id("org.jetbrains.kotlin.android") version "2.0.0"
-    id("maven-publish")
-}
-
 android {
     namespace = "com.blockshub.blockself"
     compileSdk = 34
     defaultConfig { minSdk = 24 }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     publishing { singleVariant("release") }
-}
-
-dependencies {
-    implementation("net.java.dev.jna:jna:5.14.0@aar")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.blockshub"
-            artifactId = "blockself"
-            version = project.findProperty("version") as String? ?: "0.0.0"
-            afterEvaluate { from(components["release"]) }
-        }
-    }
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/BlocksHub/Blockself")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
 }
